c8f725711f3965fbb54e09d08a322e4990c329fd
Miguel <miguel.carvajal@epn.edu.ec>
2024-10-27 22:23:42 -0500

    Examen

--- src/app/services/text.service.ts
+++ src/app/services/text.service.ts
@@ -11,53 +11,53 @@ export class TextService {
 
   constructor() {}
 
-  // Método para guardar un nuevo texto en el almacenamiento
+  
   public async addNewText(content: string) {
-    // Generar un nombre de archivo único usando la fecha actual
+    
     const fileName = Date.now() + '.txt';
 
-    // Guardar el archivo en el sistema de archivos
+   
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: content,
-      directory: Directory.Documents, // Usar Directory.Data para almacenar el archivo de texto
+      directory: Directory.Documents, 
       encoding: Encoding.UTF8
     });
 
-    // Agregar el texto a la lista en memoria con la ruta del archivo
+    
     const newText: UserText = {
       filepath: fileName,
       content: content
     };
     this.texts.unshift(newText);
 
-    // Actualizar el almacenamiento local con la nueva lista de textos
+    
     Preferences.set({
       key: this.TEXT_STORAGE,
       value: JSON.stringify(this.texts),
     });
   }
 
-  // Método para cargar los textos guardados desde el almacenamiento
+  
   public async loadSavedTexts() {
-    // Obtener la lista de textos guardados desde Preferences
+    
     const { value } = await Preferences.get({ key: this.TEXT_STORAGE });
     this.texts = (value ? JSON.parse(value) : []) as UserText[];
 
     for (let text of this.texts) {
-        // Leer el contenido del archivo
+        
         const readFile = await Filesystem.readFile({
           path: text.filepath,
           directory: Directory.Documents,
-          encoding: Encoding.UTF8 // Asegúrate de que el encoding sea UTF8
+          encoding: Encoding.UTF8 
         });
-        // Guardar el contenido en el objeto
-        text.content = readFile.data as string; // Agrega 'as string' para indicar que es texto
+        
+        text.content = readFile.data as string; 
       }
   }
 }
 
-// Definición de la interfaz UserText
+
 export interface UserText {
   filepath: string;
   content: string;
--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -7,18 +7,18 @@ import { TextService } from '../services/text.service';
   styleUrls: ['tab5.page.scss']
 })
 export class Tab5Page {
-  newText: string = ''; // Variable para el texto nuevo que se va a agregar
+  newText: string = ''; 
 
   constructor(public textService: TextService) {}
 
   addTextToStorage() {
     if (this.newText.trim().length > 0) {
       this.textService.addNewText(this.newText);
-      this.newText = ''; // Limpiar el campo de entrada
+      this.newText = ''; 
     }
   }
 
   async ngOnInit() {
-    await this.textService.loadSavedTexts(); // Cargar los textos guardados al iniciar
+    await this.textService.loadSavedTexts(); 
   }
 }
